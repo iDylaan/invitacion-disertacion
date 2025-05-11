ba06c6b1b1da5b91ecb47e265b982fade3e8431a
Danii <daniel.marin92@hotmail.com>
2025-05-11 10:33:47 -0600

    Fin invitacion

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,14 +4,19 @@ import tailwindcss from '@tailwindcss/vite'
 import react from "@astrojs/react";
 import vue from "@astrojs/vue";
 
+import netlify from "@astrojs/netlify";
+
 export default defineConfig({
-    integrations: [react(), vue()],
-    vite: {
-        plugins: [tailwindcss()],
-        resolve: {
-            alias: {
-                '@': '/src',
-            },
-        },
-    },
+  integrations: [react(), vue()],
+
+  vite: {
+      plugins: [tailwindcss()],
+      resolve: {
+          alias: {
+              '@': '/src',
+          },
+      },
+  },
+
+  adapter: netlify(),
 });
